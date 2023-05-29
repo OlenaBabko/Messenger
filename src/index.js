1382d623c50a4c89b37492ea449ce2aae4c2e8b5
ljolia <elena.babko@gmail.com>
2023-05-29 10:00:34 +0300

    app interface for browser

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Це технічний файл, який автоматично генерується технологією react.js
+// Імпортуємо потрібні функції технології react.js
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+// Імпортуємо наш додаток
+import App from "./App";
+
+// Генеруємо місце в браузері, куди ми будемо вбудовувати наш інтерфейс
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// Генеруємо наш інтерфейс в браузері
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
