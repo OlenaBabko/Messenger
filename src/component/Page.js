4e1396b667d22ec1fc3b3432717ce6854820372c
Olena Babko <elena.babko@gmail.com>
2023-06-23 09:15:10 +0300

    background cover add

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import flowersImg from "../file/photo/flowers_for_messenger_4.jpg";
+import background from "../file/photo/background_00.jpg";
 
 // Створюємо компонент "Сторінка", який відповідає за оболонку сторінки
 export default function Page({ children }) {
@@ -24,7 +25,10 @@ const Window = styled.div`
 // Елемент "Відображення" надає формат відображення інтерфейсу
 const View = styled.div`
     max-width: 620px;           /* Робимо максимальну ширину інтерфейсу додатка */
-    background-color: #efeff4;
+    background-image: url(${background});
+    background-position: center;
+    background-size: cover;
+    /* background-color: #efeff4; */
     margin: 0 auto;             /* Робимо, щоб інтерфейс ставав по центру, якщо ширина екрана більша, ніж ширина інтерфейсу */
     min-height: 100vh;          /* Задаємо мінімальну висоту інтерфейсу таку саму як висота екрана */
     display: grid;              /* Включаємо режим верстки, задаємо потрібну структуру сторінки */
